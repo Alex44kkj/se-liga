61f3396054a51f56bf165fa4be32f2d0a4b64b09
Alex44kkj <ALEX.MOREIRA.OLIVEIRA@ESCOLA.PR.GOV.BR>
2025-12-02 11:41:00 -0300

    wbhw

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function exibirMensagem() {
-  alert("Você clicou no botão!");
+function mostrarEquacao() {
+  const elemento = document.getElementById("equacao");
+
+  elemento.textContent =
+    "Equação da fotossíntese: 6 CO₂ + 6 H₂O + luz → C₆H₁₂O₆ + 6 O₂";
+
+  elemento.classList.remove("oculto");
 }
